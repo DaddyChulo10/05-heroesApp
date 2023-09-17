3234299f7b25a9bf49613585b5d0737fadbea1c9
edgar <edgtarkalim@gmail.com>
2023-09-17 11:46:03 -0600

    Rutas hijas y lazyLoading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'moduloHeroes', loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule) },
+  { path: '404', component: Error404PageComponent },
+  { path: '', redirectTo: 'moduloHeroes', pathMatch: 'full' },
+  { path: '**', redirectTo: '404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
