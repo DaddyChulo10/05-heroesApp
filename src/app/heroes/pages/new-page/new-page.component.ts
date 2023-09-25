f84a6fc1f30fd1215677825e5b2ea40af1813a91
edgar <edgtarkalim@gmail.com>
2023-09-24 21:22:34 -0600

    Crud Heroes

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+// import { Hero } from './../../interfaces/hero.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
+
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
+
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
@@ -6,9 +18,112 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
+
+  constructor(
+    private heroService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) { }
+
+
+  ngOnInit(): void {
+    if (!this.router.url.includes('edit')) return
+
+    this.activatedRoute.params
+      .pipe(
+        //Obtener el id de la URL
+        switchMap(({ id }) => this.heroService.getHeroById(id))
+      )
+      .subscribe(hero => {
+        if (!hero) return this.router.navigateByUrl('/')
+        //reset() asigna los valores como en el formulario, interfaces = formControlName 
+        this.heroForm.reset(hero)
+        return
+
+      })
+  }
+
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.MarvelComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  })
+
+  get CurrentHero(): Hero {
+    const hero = this.heroForm.value as Hero
+    return hero;
+  }
+
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
+    //  this.heroService.updateHero(this.heroForm.value)
+
+    if (this.CurrentHero.id) {
+      this.heroService.updateHero(this.CurrentHero)
+        .subscribe(hero => {
+          this.router.navigateByUrl('/')
+          this.showSnackbar(` ${hero.superhero} Actualizado! `)
+        })
+      return
+
+    }
+
+    this.heroService.addHero(this.CurrentHero)
+      .subscribe(hero => {
+        this.router.navigate(['moduloHeroes/submoduloHero/edit', hero.id])
+        this.showSnackbar(` ${hero.superhero} Creado! `)
+      })
+
+    // console.log({
+    //   formIsValid: this.heroForm.valid, 
+    //   value: this.heroForm.value
+    // })
+  }
+
+  showSnackbar(message: string): void {
+    this.snackBar.open(message, 'Done', {
+      duration: 3500,
+    })
+  }
+
+  onDeleteHero() {
+    if (!this.CurrentHero.id) throw Error('hero id is required')
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value
+    });
+
+    dialogRef.afterClosed()
+      .subscribe(result => {
+        if (!result) return
+
+        this.heroService.deleteHeroById(this.CurrentHero.id)
+          .subscribe(wasDeleted => {
+            if (wasDeleted)
+              this.router.navigate(['/moduloHeroes/submoduloHero/list'])
+
+          })
+
+
+        // console.log({ result });
+        // this.animal = result;
+      });
+
+
+  }
+
+
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
+
+
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, map, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -11,25 +11,51 @@ export class HeroesService {
 
   private baseUrl: string = environments.baseUrl
 
-  constructor( private http: HttpClient  ) { }
+  constructor(private http: HttpClient) { }
 
 
   //Consulta de la bd
-  getHeroes():Observable<Hero[]>{
+  getHeroes(): Observable<Hero[]> {
     // console.log(`${this.baseUrl}/heroes`)
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
   }
 
   //buscar por id, undefined es cuando no existe el id
-  getHeroById(id: string):Observable<Hero | undefined>{
+  getHeroById(id: string): Observable<Hero | undefined> {
     return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
-    //CAPTURA EL ERROR SI NO EXISTE LA INFORMACION
-    .pipe(
-      catchError( error => of(undefined))
-    )
+      //CAPTURA EL ERROR SI NO EXISTE LA INFORMACION
+      .pipe(
+        catchError(error => of(undefined))
+      )
   }
 
-  getSuggestions(query:string): Observable<Hero[]>{
+  //Buscar por sugerencias <Buscador></Buscador>
+  getSuggestions(query: string): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`)
   }
+
+
+  //Agregar un nuevo elemento
+  addHero(hero: Hero): Observable<Hero> {
+    //El hero va ser el body del backend
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero)
+  }
+
+  // actualizar un nuevoi elemento "por registros"
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('El Hero.id is required')
+    //El patch es para cambiar parte del registro
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero)
+  }
+
+  //elimina registro por id
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        map(resp => true),
+        catchError(err => of(false)),
+      )
+  }
+
+
 }
